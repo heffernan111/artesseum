bac2376ff7eb49b5a753b890d3488f12f630b4c7
David Heffernan <david.heffernan@datatank.co.uk>
2022-01-09 12:36:28 +0000

    electron links

--- src/components/ElectronBoilerPlateCard.js
+++ src/components/ElectronBoilerPlateCard.js
@@ -28,39 +28,39 @@ const ElectronBoilerPlateCard = () => {
                     <IoLogoElectron fontSize="1.2em"/> MERN Electron.js Stack
                 </div>
                 <div className="card-body">
-                    <p className="code-text">Clone the boiler plate</p>
+                    <p className="code-text">Clone the boiler plate</p>a
                     <code>
                         <span className="dollar">$</span>
-                        <span id="code_1">git clone https://github.com/heffman11/mern_electron.git &lt;folder_name&gt;</span>
-                        <span class="icon" onClick={() => copyCode('code_1')}><MdCopyAll fontSize="1.2em"/></span>
+                        <span id="electron_1">git clone https://github.com/heffman11/mern_electron.git &lt;folder_name&gt;</span>
+                        <span class="icon" onClick={() => copyCode('electron_1')}><MdCopyAll fontSize="1.2em"/></span>
                     </code>
 
                     <p className="code-text">Enter folder</p>
                     <code>
                         <span className="dollar">$</span>
-                        <span id="code_2">cd &lt;folder_name&gt;</span>
-                        <span class="icon" onClick={() => copyCode('code_2')}><MdCopyAll fontSize="1.2em"/></span>
+                        <span id="electron_2">cd &lt;folder_name&gt;</span>
+                        <span class="icon" onClick={() => copyCode('electron_2')}><MdCopyAll fontSize="1.2em"/></span>
                     </code>
 
                     <p className="code-text">Artesseum is split into frontend (React) and backend (Node.js).</p>
                     <code>
                         <span className="dollar">$</span>
-                        <span id="code_3">cd frontend</span>
-                        <span class="icon" onClick={() => copyCode('code_3')}><MdCopyAll fontSize="1.2em"/></span>
+                        <span id="electron_3">cd frontend</span>
+                        <span class="icon" onClick={() => copyCode('electron_3')}><MdCopyAll fontSize="1.2em"/></span>
                     </code>
 
                     <p className="code-text">Install dependencies</p>
                     <code>
                         <span className="dollar">$</span>
-                        <span id="code_4">npm install</span>
-                        <span class="icon" onClick={() => copyCode('code_4')}><MdCopyAll fontSize="1.2em"/></span>
+                        <span id="electron_4">npm install</span>
+                        <span class="icon" onClick={() => copyCode('electron_4')}><MdCopyAll fontSize="1.2em"/></span>
                     </code>
 
                     <p className="code-text">You can now start your front end app.</p>
                     <code>
                         <span className="dollar">$</span>
-                        <span id="code_5">npm run electron:start</span>
-                        <span class="icon" onClick={() => copyCode('code_5')}><MdCopyAll fontSize="1.2em"/></span>
+                        <span id="electron_5">npm run electron:start</span>
+                        <span class="icon" onClick={() => copyCode('electron_5')}><MdCopyAll fontSize="1.2em"/></span>
                     </code>
 
                     <p className="code-text">Install backend dependencies</p>
@@ -72,22 +72,22 @@ const ElectronBoilerPlateCard = () => {
                     <br/>
                     <code>
                         <span className="dollar">$</span>
-                        <span id="code_7">cd backend</span>
-                        <span class="icon" onClick={() => copyCode('code_7')}><MdCopyAll fontSize="1.2em"/></span>
+                        <span id="electron_7">cd backend</span>
+                        <span class="icon" onClick={() => copyCode('electron_7')}><MdCopyAll fontSize="1.2em"/></span>
                     </code>
 
                     <p className="code-text">Install dependencies</p>
                     <code>
                         <span className="dollar">$</span>
-                        <span id="code_8">npm install</span>
-                        <span class="icon" onClick={() => copyCode('code_8')}><MdCopyAll fontSize="1.2em"/></span>
+                        <span id="electron_8">npm install</span>
+                        <span class="icon" onClick={() => copyCode('electron_8')}><MdCopyAll fontSize="1.2em"/></span>
                     </code>
 
                     <p className="code-text">You can now start your node server.</p>
                     <code>
                         <span className="dollar">$</span>
-                        <span id="code_9">npm start</span>
-                        <span class="icon" onClick={() => copyCode('code_9')}><MdCopyAll fontSize="1.2em"/></span>
+                        <span id="electron_9">npm start</span>
+                        <span class="icon" onClick={() => copyCode('electron_9')}><MdCopyAll fontSize="1.2em"/></span>
                     </code>
 
                     <div className="text-center pt-4">
